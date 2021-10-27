c4a5a835f695918e389deb520c483dc0d6c3592f
Fei Chen <fechen@nvidia.com>
2021-10-27 22:09:08 +0800

    Update for DeepStream SDK 6.0 GA

--- deepstream_app_tao_configs/download_models.sh
+++ deepstream_app_tao_configs/download_models.sh
@@ -29,7 +29,7 @@ echo "==================================================================="
 echo "begin download models for Faster-RCNN / YoloV3 / YoloV4 /SSD / DSSD / RetinaNet/ UNET/"
 echo "==================================================================="
 mkdir -p ../../models/tao_pretrained_models
-wget https://nvidia.box.com/shared/static/i1cer4s3ox4v8svbfkuj5js8yqm3yazo.zip -O models.zip
+wget https://nvidia.box.com/shared/static/511552h6b1ecw4gd20ptuihoiidz13cs -O models.zip
 unzip models.zip -d ../../models/tao_pretrained_models
 mv ../../models/tao_pretrained_models/models/* ../../models/tao_pretrained_models/
 rm -r ../../models/tao_pretrained_models/models
@@ -40,47 +40,47 @@ echo "begin download models for dashcamnet / vehiclemakenet / vehicletypenet"
 echo " / trafficcamnet / facedetectir"
 echo "==================================================================="
 mkdir -p ../../models/tao_pretrained_models/dashcamnet && \
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/dashcamnet/versions/pruned_v1.0/zip \
--O dashcamnet_pruned_v1.0.zip && unzip dashcamnet_pruned_v1.0.zip -d ../../models/tao_pretrained_models/dashcamnet \
-&& rm dashcamnet_pruned_v1.0.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/dashcamnet/versions/pruned_v1.0.1/zip \
+-O dashcamnet_pruned_v1.0.1.zip && unzip dashcamnet_pruned_v1.0.1.zip -d ../../models/tao_pretrained_models/dashcamnet \
+&& rm dashcamnet_pruned_v1.0.1.zip
 mkdir -p ../../models/tao_pretrained_models/vehiclemakenet && \
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/vehiclemakenet/versions/pruned_v1.0/zip \
--O vehiclemakenet_pruned_v1.0.zip && \
-unzip vehiclemakenet_pruned_v1.0.zip -d ../../models/tao_pretrained_models/vehiclemakenet && \
-rm vehiclemakenet_pruned_v1.0.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/vehiclemakenet/versions/pruned_v1.0.1/zip \
+-O vehiclemakenet_pruned_v1.0.1.zip && \
+unzip vehiclemakenet_pruned_v1.0.1.zip -d ../../models/tao_pretrained_models/vehiclemakenet && \
+rm vehiclemakenet_pruned_v1.0.1.zip
 mkdir -p ../../models/tao_pretrained_models/vehicletypenet && \
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/vehicletypenet/versions/pruned_v1.0/zip \
--O vehicletypenet_pruned_v1.0.zip && \
-unzip vehicletypenet_pruned_v1.0.zip -d ../../models/tao_pretrained_models/vehicletypenet && \
-rm vehicletypenet_pruned_v1.0.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/vehicletypenet/versions/pruned_v1.0.1/zip \
+-O vehicletypenet_pruned_v1.0.1.zip && \
+unzip vehicletypenet_pruned_v1.0.1.zip -d ../../models/tao_pretrained_models/vehicletypenet && \
+rm vehicletypenet_pruned_v1.0.1.zip
 mkdir -p ../../models/tao_pretrained_models/trafficcamnet && \
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/trafficcamnet/versions/pruned_v1.0/zip \
--O trafficcamnet_pruned_v1.0.zip && \
-unzip trafficcamnet_pruned_v1.0.zip -d ../../models/tao_pretrained_models/trafficcamnet && \
-rm trafficcamnet_pruned_v1.0.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/trafficcamnet/versions/pruned_v1.0.1/zip \
+-O trafficcamnet_pruned_v1.0.1.zip && \
+unzip trafficcamnet_pruned_v1.0.1.zip -d ../../models/tao_pretrained_models/trafficcamnet && \
+rm trafficcamnet_pruned_v1.0.1.zip
 mkdir -p ../../models/tao_pretrained_models/facedetectir && \
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/facedetectir/versions/pruned_v1.0/zip \
--O facedetectir_pruned_v1.0.zip && \
-unzip facedetectir_pruned_v1.0.zip -d ../../models/tao_pretrained_models/facedetectir && \
-rm facedetectir_pruned_v1.0.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/facedetectir/versions/pruned_v1.0.1/zip \
+-O facedetectir_pruned_v1.0.1.zip && \
+unzip facedetectir_pruned_v1.0.1.zip -d ../../models/tao_pretrained_models/facedetectir && \
+rm facedetectir_pruned_v1.0.1.zip
 
 echo "==================================================================="
 echo "begin download models for peopleNet "
 echo "==================================================================="
 mkdir -p ../../models/tao_pretrained_models/peopleNet/V2.1
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/peoplenet/versions/pruned_quantized_v2.1/zip \
--O peoplenet_pruned_int8_v2.1.zip && \
-unzip peoplenet_pruned_int8_v2.1.zip -d ../../models/tao_pretrained_models/peopleNet/V2.1 && \
-rm peoplenet_pruned_int8_v2.1.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/peoplenet/versions/pruned_quantized_v2.1.1/zip \
+-O peoplenet_pruned_int8_v2.1.1.zip && \
+unzip peoplenet_pruned_int8_v2.1.1.zip -d ../../models/tao_pretrained_models/peopleNet/V2.1 && \
+rm peoplenet_pruned_int8_v2.1.1.zip
 
 echo "==================================================================="
 echo "begin download models for peopleSegNet "
 echo "==================================================================="
 mkdir -p ../../models/tao_pretrained_models/peopleSegNet/V2
-wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/peoplesegnet/versions/deployable_v2.0/zip \
--O peoplesegnet_deployable_v2.0.zip && \
-unzip peoplesegnet_deployable_v2.0.zip -d ../../models/tao_pretrained_models/peopleSegNet/V2 && \
-rm peoplesegnet_deployable_v2.0.zip
+wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/peoplesegnet/versions/deployable_v2.0.1/zip \
+-O peoplesegnet_deployable_v2.0.1.zip && \
+unzip peoplesegnet_deployable_v2.0.1.zip -d ../../models/tao_pretrained_models/peopleSegNet/V2 && \
+rm peoplesegnet_deployable_v2.0.1.zip
 
 wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tao/peoplesegnet/versions/deployable_v1.0/zip \
 -O peoplesegnet_deployable_v1.0.zip && \
